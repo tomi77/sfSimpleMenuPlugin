4968f7abd3c39bb290a360ba0e166bc84ee277d6
tomi <tomi@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-07-05 20:19:44 +0000

    Simpler to create a menu item

--- modules/sfSimpleMenu/templates/_ul.php
+++ modules/sfSimpleMenu/templates/_ul.php
@@ -1,11 +1,7 @@
 <ul class="sf_simple_menu">
   <?php foreach ($Menu as $Item): ?>
   <li class="sf_simple_menu_item">
-    <?php if ($Item->hasURL()): ?>
-    <a href="<?php echo url_for($Item->getURL()) ?>" class="sf_simple_menu_link"><?php echo $Item->getTitle() ?></a>
-    <?php else: ?>
-    <?php echo $Item->getTitle() ?>
-    <?php endif; ?>
+    <?php echo $Item->hasURL() ? link_to($Item->getTitle(), $Item->getURL(), array('class' => 'sf_simple_menu_link')) : $Item->getTitle() ?>
     <?php if ($Item->hasSubmenu()): ?>
     <?php include_partial('sfSimpleMenu/ul', array('Menu' => $Item->getSubmenu())) ?>
     <?php endif; ?>
