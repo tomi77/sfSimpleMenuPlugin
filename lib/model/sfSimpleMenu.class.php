a13b8901f144a43f741f8b272f3f730b8b86fec9
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2019-01-01 10:52:52 +0000

    Apply fixes from StyleCI

--- lib/model/sfSimpleMenu.class.php
+++ lib/model/sfSimpleMenu.class.php
@@ -1,100 +1,96 @@
 <?php
 
 /**
- * One menu level
+ * One menu level.
  *
- * @package    sfSimpleMenuPlugin
- * @subpackage model
  * @author     Tomasz Jakub Rup <tomasz.rup@gmail.com>
  */
 class sfSimpleMenu implements ArrayAccess, Iterator
 {
+    /**
+     * @var array[sfMenuItem]
+     */
+    private $menu = array();
 
-  /**
-   *
-   * @var array[sfMenuItem]
-   */
-  private $menu = array();
-  
-  /**
-   * Current offset for iterator
-   * @var integer
-   */
-  private $currentOffset = 0;
+    /**
+     * Current offset for iterator.
+     *
+     * @var int
+     */
+    private $currentOffset = 0;
 
-  public function __construct(array $menuItems)
-  {
-    foreach ($menuItems as $menuItem) {
-      if (isset($menuItem['credentials']) && !sfContext::getInstance()->getUser()->hasCredential($menuItem['credentials'])) {
-        continue;
-      }
-      $this->menu[] = new sfSimpleMenuItem($menuItem);
-    }
-  }
-  
-  /*
-   * ArrayAccess interface
-   */
-
-  public function offsetExists($offset)
-  {
-    return isset($this->menu[$offset]);
-  }
-
-  public function offsetGet($offset)
-  {
-    return $this->menu[$offset];
-  }
-
-  public function offsetSet($offset, $value)
-  {
-    $this->menu[$offset] = $value;
-  }
-
-  public function offsetUnset($offset)
-  {
-    unset($this->menu[$offset]);
-  }
-  
-  /*
-   * Iterator interface
-   */
-
-  public function current()
-  {
-    return $this->menu[$this->currentOffset];
-  }
-
-  public function next()
-  {
-    $keys = array_keys($this->menu);
-    $next = null;
-    foreach ($keys as $idx => $key) {
-      if ($key == $this->currentOffset) {
-        $next = $idx + 1;
-      }
-    }
-    if ($next) {
-      $this->currentOffset = isset($keys[$next]) ? $keys[$next] : null;
+    public function __construct(array $menuItems)
+    {
+        foreach ($menuItems as $menuItem) {
+            if (isset($menuItem['credentials']) && !sfContext::getInstance()->getUser()->hasCredential($menuItem['credentials'])) {
+                continue;
+            }
+            $this->menu[] = new sfSimpleMenuItem($menuItem);
+        }
     }
 
-    return $this->currentOffset;
-  }
+    /*
+     * ArrayAccess interface
+     */
 
-  public function key()
-  {
-    return $this->currentOffset;
-  }
+    public function offsetExists($offset)
+    {
+        return isset($this->menu[$offset]);
+    }
 
-  public function valid()
-  {
-    return isset($this->menu[$this->currentOffset]);
-  }
+    public function offsetGet($offset)
+    {
+        return $this->menu[$offset];
+    }
 
-  public function rewind()
-  {
-    $keys = array_keys($this->menu);
-    $this->currentOffset = $keys[0];
-  }
+    public function offsetSet($offset, $value)
+    {
+        $this->menu[$offset] = $value;
+    }
 
+    public function offsetUnset($offset)
+    {
+        unset($this->menu[$offset]);
+    }
+
+    /*
+     * Iterator interface
+     */
+
+    public function current()
+    {
+        return $this->menu[$this->currentOffset];
+    }
+
+    public function next()
+    {
+        $keys = array_keys($this->menu);
+        $next = null;
+        foreach ($keys as $idx => $key) {
+            if ($key == $this->currentOffset) {
+                $next = $idx + 1;
+            }
+        }
+        if ($next) {
+            $this->currentOffset = isset($keys[$next]) ? $keys[$next] : null;
+        }
+
+        return $this->currentOffset;
+    }
+
+    public function key()
+    {
+        return $this->currentOffset;
+    }
+
+    public function valid()
+    {
+        return isset($this->menu[$this->currentOffset]);
+    }
+
+    public function rewind()
+    {
+        $keys = array_keys($this->menu);
+        $this->currentOffset = $keys[0];
+    }
 }
--- lib/model/sfSimpleMenuItem.class.php
+++ lib/model/sfSimpleMenuItem.class.php
@@ -1,59 +1,55 @@
 <?php
 
 /**
- * Menu item (with submenu)
+ * Menu item (with submenu).
  *
- * @package    sfSimpleMenuPlugin
- * @subpackage model
  * @author     Tomasz Jakub Rup <tomasz.rup@gmail.com>
  */
 class sfSimpleMenuItem
 {
+    private $title;
 
-  private $title;
-  
-  private $url;
-  
-  private $submenu;
-  
-  private $isSeparator;
-  
-  public function __construct(array $menuItem)
-  {
-    $this->title = isset($menuItem['title']) ? $menuItem['title'] : '';
-    $this->url = isset($menuItem['url']) ? $menuItem['url'] : null;
-    $this->submenu = isset($menuItem['submenu']) ? new sfSimpleMenu($menuItem['submenu']) : null;
-    $this->isSeparator = isset($menuItem['separator']);
-  }
+    private $url;
 
-  public function getTitle()
-  {
-    return sfContext::getInstance()->getI18N()->__($this->title, array(), 'sf_menu');
-  }
+    private $submenu;
 
-  public function hasURL()
-  {
-    return $this->url !== null;
-  }
+    private $isSeparator;
 
-  public function getURL()
-  {
-    return $this->url;
-  }
+    public function __construct(array $menuItem)
+    {
+        $this->title = isset($menuItem['title']) ? $menuItem['title'] : '';
+        $this->url = isset($menuItem['url']) ? $menuItem['url'] : null;
+        $this->submenu = isset($menuItem['submenu']) ? new sfSimpleMenu($menuItem['submenu']) : null;
+        $this->isSeparator = isset($menuItem['separator']);
+    }
 
-  public function hasSubmenu()
-  {
-    return $this->submenu !== null;
-  }
+    public function getTitle()
+    {
+        return sfContext::getInstance()->getI18N()->__($this->title, array(), 'sf_menu');
+    }
 
-  public function getSubmenu()
-  {
-    return $this->submenu;
-  }
-  
-  public function isSeparator()
-  {
-    return $this->isSeparator;
-  }
+    public function hasURL()
+    {
+        return null !== $this->url;
+    }
 
+    public function getURL()
+    {
+        return $this->url;
+    }
+
+    public function hasSubmenu()
+    {
+        return null !== $this->submenu;
+    }
+
+    public function getSubmenu()
+    {
+        return $this->submenu;
+    }
+
+    public function isSeparator()
+    {
+        return $this->isSeparator;
+    }
 }
